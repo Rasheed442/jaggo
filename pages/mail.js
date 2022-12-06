9277cea433a7b2951a3b1b0ca56a5af1fb398160
Rasheed <abdulraufrasheed15@gmail.com>
2022-12-06 08:30:24 +0100

    ch

--- pages/mail.js
+++ pages/mail.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import style from '../styles/mail.module.css'
 import {FiSettings} from "react-icons/fi"
 import {AiOutlineSearch} from "react-icons/ai"
+import {RiMailAddLine} from 'react-icons/ri'
 function Mail() {
   return (
     <div className={style.container}>
@@ -19,8 +20,201 @@ function Mail() {
 
         <div className={style.search}>
           <AiOutlineSearch size={25} style={{color:"white"}}/>
-           <input type='text' placeholder='search direct message'/>
+           <input type='text' placeholder='search messages'/>
         </div>
+        <hr/>
+
+             <div className={style.request}>
+                 <RiMailAddLine size={20}/>
+                 <p>Message requests</p>
+             </div>
+
+
+             <div className={style.line}>
+                 <Image
+                   priority
+                   src='/snik.png'
+                   width={35}
+                   height={35}
+                   />
+                   <div className={style.obs}>
+                      <h5>JAGGO Sport </h5>
+                      <p>What do you think about today's game Barca vs Bayern &#128293;</p>
+                   </div>
+              </div>
+
+             <div className={style.line}>
+                 <Image
+                   priority
+                   src='/snik.png'
+                   width={35}
+                   height={35}
+                   />
+                   <div className={style.obs}>
+                      <h5>JAGGO Sport </h5>
+                      <p>What do you think about today's game Barca vs Bayern &#128293;</p>
+                   </div>
+              </div>
+
+             <div className={style.line}>
+                 <Image
+                   priority
+                   src='/snik.png'
+                   width={35}
+                   height={35}
+                   />
+                   <div className={style.obs}>
+                      <h5>JAGGO Sport </h5>
+                      <p>What do you think about today's game Barca vs Bayern &#128293;</p>
+                   </div>
+              </div>
+
+             <div className={style.line}>
+                 <Image
+                   priority
+                   src='/snik.png'
+                   width={35}
+                   height={35}
+                   />
+                   <div className={style.obs}>
+                      <h5>JAGGO Sport </h5>
+                      <p>What do you think about today's game Barca vs Bayern &#128293;</p>
+                   </div>
+              </div>
+
+             <div className={style.line}>
+                 <Image
+                   priority
+                   src='/snik.png'
+                   width={35}
+                   height={35}
+                   />
+                   <div className={style.obs}>
+                      <h5>JAGGO Sport </h5>
+                      <p>What do you think about today's game Barca vs Bayern &#128293;</p>
+                   </div>
+              </div>
+
+             <div className={style.line}>
+                 <Image
+                   priority
+                   src='/snik.png'
+                   width={35}
+                   height={35}
+                   />
+                   <div className={style.obs}>
+                      <h5>JAGGO Sport </h5>
+                      <p>What do you think about today's game Barca vs Bayern &#128293;</p>
+                   </div>
+              </div>
+
+             <div className={style.line}>
+                 <Image
+                   priority
+                   src='/snik.png'
+                   width={35}
+                   height={35}
+                   />
+                   <div className={style.obs}>
+                      <h5>JAGGO Sport </h5>
+                      <p>What do you think about today's game Barca vs Bayern &#128293;</p>
+                   </div>
+              </div>
+
+             <div className={style.line}>
+                 <Image
+                   priority
+                   src='/snik.png'
+                   width={35}
+                   height={35}
+                   />
+                   <div className={style.obs}>
+                      <h5>JAGGO Sport </h5>
+                      <p>What do you think about today's game Barca vs Bayern &#128293;</p>
+                   </div>
+              </div>
+
+             <div className={style.line}>
+                 <Image
+                   priority
+                   src='/snik.png'
+                   width={35}
+                   height={35}
+                   />
+                   <div className={style.obs}>
+                      <h5>JAGGO Sport </h5>
+                      <p>What do you think about today's game Barca vs Bayern &#128293;</p>
+                   </div>
+              </div>
+
+             <div className={style.line}>
+                 <Image
+                   priority
+                   src='/snik.png'
+                   width={35}
+                   height={35}
+                   />
+                   <div className={style.obs}>
+                      <h5>JAGGO Sport </h5>
+                      <p>What do you think about today's game Barca vs Bayern &#128293;</p>
+                   </div>
+              </div>
+
+             <div className={style.line}>
+                 <Image
+                   priority
+                   src='/snik.png'
+                   width={35}
+                   height={35}
+                   />
+                   <div className={style.obs}>
+                      <h5>JAGGO Sport </h5>
+                      <p>What do you think about today's game Barca vs Bayern &#128293;</p>
+                   </div>
+              </div>
+
+             <div className={style.line}>
+                 <Image
+                   priority
+                   src='/snik.png'
+                   width={35}
+                   height={35}
+                   />
+                   <div className={style.obs}>
+                      <h5>JAGGO Sport </h5>
+                      <p>What do you think about today's game Barca vs Bayern &#128293;</p>
+                   </div>
+              </div>
+
+             <div className={style.line}>
+                 <Image
+                   priority
+                   src='/snik.png'
+                   width={35}
+                   height={35}
+                   />
+                   <div className={style.obs}>
+                      <h5>JAGGO Sport </h5>
+                      <p>What do you think about today's game Barca vs Bayern &#128293;</p>
+                   </div>
+              </div>
+
+             <div className={style.line}>
+                 <Image
+                   priority
+                   src='/snik.png'
+                   width={35}
+                   height={35}
+                   />
+                   <div className={style.obs}>
+                      <h5>JAGGO Sport </h5>
+                      <p>What do you think about today's game Barca vs Bayern &#128293;</p>
+                   </div>
+              </div>
+
+
+
+
     </div>
   )
 }
